8d503acfc595ea0c20691b1f46cf62a49678ec31
Syphurus <146368398+Syphurus@users.noreply.github.com>
2025-04-13 09:39:36 +0530

    Fonts, assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1 className="h1-bold">Welcome to the world of Nextjs</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Nextjs</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to the world of Nextjs</h1>
+    </>
+  );
 };
 
 export default Home;
